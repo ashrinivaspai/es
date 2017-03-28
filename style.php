34ee60721a3e0c33b3703ac865e97af6d41c071c
ashrinivaspai <ashrinivaspai@gmail.com>
2017-03-28 20:58:22 +0530

    redundant
    using w3.css

---
+++ style.php
@@ -0,0 +1,113 @@
+<style>
+#attendance {
+    font-family: "Trebuchet MS", Arial, Helvetica, sans-serif;
+    border-collapse: collapse;
+    width: 100%;
+}
+
+#attendance td, #attendance th {
+    border: 1px solid #ddd;
+    padding: 6px;
+}
+
+#attendance tr:nth-child(even){background-color: #f2f2f2;}
+
+#attendance tr:hover {background-color: #ddd;}
+
+#attendance th {
+    padding-top: 12px;
+    padding-bottom: 12px;
+    text-align: left;
+    background-color: #9999ff;
+    color: white;
+}
+.button
+{
+    background-color:#9999ff;
+    border:none;
+    box-shadow: 0px 1px 1px rgba(255,255,255,0.8) inset, 1px 1px 3px rgba(0,0,0,0.2);
+    color:white;
+    padding: 10px 25px;
+    text-align: center;
+    text-decoration: none;
+    display: inline-block;
+    font-size: 16px;
+    cursor:pointer;
+    border-radius: 0px;
+    transition-duration: 0.3s;
+    transition-delay:0.0s;
+}
+.disabled
+{
+opacity: 0.6;
+cursor: not-allowed;
+}
+.button1:hover
+{
+    background-color:#fff;
+    color:#9999ff;
+    box-shadow:1px 1px 1px rgba(255,255,255,0) outset,3px 3px 3px rgba(0,0,0,0.2);
+}
+input[type=text]
+{
+    padding: 0;
+    height: 25px;
+    position: relative;
+    left: 0;
+    outline: none;
+    border: 1px solid #cdcdcd;
+    border-color: rgba(0,0,0,.15);
+    background-color: white;
+    font-size: 16px;
+}
+input[type=number]
+{
+    padding: 0;
+    height: 25px;
+    position: relative;
+    left: 0;
+    outline: none;
+    border: 1px solid #cdcdcd;
+    border-color: rgba(0,0,0,.15);
+    background-color: white;
+    font-size: 16px;
+}
+input[type=date]
+{
+    padding: 0;
+    height: 25px;
+    position: relative;
+    left: 0;
+    outline: none;
+    border: 1px solid #cdcdcd;
+    border-color: rgba(0,0,0,.15);
+    background-color: white;
+    font-size: 16px;
+}
+select
+{
+    padding: 0;
+    height: 25px;
+    position: relative;
+    left: 0;
+    outline: none;
+    border: 1px solid #cdcdcd;
+    border-color: rgba(0,0,0,.15);
+    background-color: white;
+    font-size: 16px;
+}
+input[type=password]
+{
+    padding: 0;
+    height: 25px;
+    position: relative;
+    left: 0;
+    outline: none;
+    border: 1px solid #cdcdcd;
+    border-color: rgba(0,0,0,.15);
+    background-color: white;
+    font-size: 16px;
+}
+
+
+</style>
